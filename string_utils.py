5bfde5f4ced993204e27ded68b9cf50daa620aba
1tsZak <148216415+1tsZak@users.noreply.github.com>
2025-11-18 22:11:20 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -3,12 +3,13 @@ def split_before_each_uppercases(formula):
     y=[]
     for i in formula:
         if x=="":
-            x=+i
+            x+=i
         elif i.isupper():
             y.append(x)
             x=""
             x+=i
         else: x+=i
+    y.append(x)
     return(y)
 
 
